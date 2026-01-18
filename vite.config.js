6e83b020e35922cdc9b98337623d47c84c3aa719
Michael Ehrich <michael.ehrich@krekeny.com>
2026-01-18 22:01:48 +0100

    feat(workflows): add deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/interactive-presenter/',
   server: {
     host: true,
   },
